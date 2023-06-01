d9c94ec2b98faf384b4c70d11c7158491fba15e4
JaMorant_1 <JaMorant_1@qq.com>
2023-06-01 14:08:49 +0800

    第五次提交

--- service_gateway/src/main/java/com/JaMorant/SSM/gateway/ApiGatewayApplication.java
+++ service_gateway/src/main/java/com/JaMorant/SSM/gateway/ApiGatewayApplication.java
@@ -7,6 +7,7 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 @SpringBootApplication
 @EnableDiscoveryClient
 public class ApiGatewayApplication {
+
     public static void main(String[] args) {
         SpringApplication.run(ApiGatewayApplication.class, args);
     }
